2b60f18ad79488cea9d685d604b73105569b986f
Oisin Prendiville <oisin@supertop.co>
2016-04-14 09:20:39 +0300

    Initial Commit

---
+++ PresentModallyCurrentContext/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PresentModallyCurrentContext
+//
+//  Created by Oisin Prendiville on 14/04/2016.
+//  Copyright © 2016 Oisin Prendiville. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PresentModallyCurrentContextTests/PresentModallyCurrentContextTests.m
@@ -0,0 +1,39 @@
+//
+//  PresentModallyCurrentContextTests.m
+//  PresentModallyCurrentContextTests
+//
+//  Created by Oisin Prendiville on 14/04/2016.
+//  Copyright © 2016 Oisin Prendiville. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PresentModallyCurrentContextTests : XCTestCase
+
+@end
+
+@implementation PresentModallyCurrentContextTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
